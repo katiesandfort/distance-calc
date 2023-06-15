371fd7ec218a948d2c6d7062fafb7267fb64d581
Katie Sandfort <katie.sandfort@gmail.com>
2023-06-14 18:48:17 -0700

    starting to add Esri Leaflet

--- client/index.js
+++ client/index.js
@@ -5,70 +5,109 @@ import styles from './scss/application.scss';
 const map = L.map('map').setView([47.6777, -122.338], 11);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 
-//calculate distance
+ // create the geocoding control and add it to the map
+//  var searchControl = L.esri.Geocoding.geosearch({
+//   providers: [
+//     L.esri.Geocoding.arcgisOnlineProvider({
+//       // API Key to be passed to the ArcGIS Online Geocoding Service
+//       apikey: 'YOUR_API_KEY'
+//     })
+//   ]
+// }).addTo(map);
+
+// // create an empty layer group to store the results and add it to the map
+// var results = L.layerGroup().addTo(map);
+
+// // listen for the results event and add every result to the map
+// searchControl.on("results", function (data) {
+//   results.clearLayers();
+//   for (var i = data.results.length - 1; i >= 0; i--) {
+//     results.addLayer(L.marker(data.results[i].latlng));
+//   }
+// });
+
+//initialize variables
 let startPoint;
 let endPoint;
-let _polyline;
+let polyline;
 let markerA = null;
 let markerB = null;
 
-//get button
+function getDistance(start, end) {
+  polyline = L.polyline([start, end], {
+    color:"purple"
+  }).addTo(map);
+  let length = map.distance(start, end);
+  length = length/1000
+  document.getElementById("distance").innerHTML = `${length.toFixed(2)}km`;
+}
+
+function placeMarker(point) {
+  return L.marker(point).addTo(map);
+}
+
+//get form
 const form = document.getElementById('form');
 
+//listen for form submission
 form.addEventListener('submit', (e) => {
+  //prevent default
   e.preventDefault();
 
+  //clear map if previous entries are present
   if (startPoint && endPoint) {
     map.removeLayer(markerA);
     map.removeLayer(markerB);
-    map.removeLayer(_polyline);
-      _polyline = null;
+    map.removeLayer(polyline);
+    polyline = null;
   }
 
+  //get values from form
   const pointA = document.getElementById('pointA');
   const pointB = document.getElementById('pointB');
   const stringA = pointA.value;
+  const stringB = pointB.value;
+
+  //format starting point
   const pointAArr = stringA.split(',');
   startPoint = {
     lat: pointAArr[0],
     lng: pointAArr[1]
   }
 
-  const stringB = pointB.value;
+  //formate ending point
   const pointBArr = stringB.split(',');
   endPoint = {
     lat: pointBArr[0],
     lng: pointBArr[1]
   }
 
-  markerA = L.marker(startPoint).addTo(map);
-  markerB = L.marker(endPoint).addTo(map);
-  _polyline = L.polyline([startPoint, endPoint], {
-    color:"purple"
-  }).addTo(map);
-  let length = map.distance(startPoint, endPoint);
-  length = length/1000
-  document.getElementById("distance").innerHTML = `${length}kms`;
+  //place markers
+  markerA = placeMarker(startPoint);
+  markerB = placeMarker(endPoint);
+  
+  //get distance
+  getDistance(startPoint, endPoint);
 })
 
+//extra functionality for clicking map
 map.on('click', (e) => {
+  //first click
   if(!startPoint){
     startPoint = e.latlng;
-    console.log(startPoint);
-    markerA = L.marker(startPoint).addTo(map);
-  }else if (!endPoint){
+    markerA = placeMarker(startPoint);
+  }
+  //second click
+  else if (!endPoint){
     endPoint = e.latlng;
-    markerB = L.marker(endPoint).addTo(map);
-    _polyline = L.polyline([startPoint, endPoint], {
-      color:"purple"
-    }).addTo(map);
-    let length = map.distance(startPoint, endPoint);
-    length = length/1000
-    document.getElementById("distance").innerHTML = `${length}kms`;
-  }else{
-    if (_polyline){
-      map.removeLayer(_polyline);
-      _polyline = null;
+    markerB = placeMarker(endPoint);
+    getDistance(startPoint, endPoint);
+  }
+  //third click
+  else{
+    if (polyline){
+      map.removeLayer(polyline);
+      polyline = null;
     }
     startPoint = e.latlng;
     endPoint = null;
@@ -76,4 +115,6 @@ map.on('click', (e) => {
     map.removeLayer(markerB);
     markerA = L.marker(startPoint).addTo(map)
   }
-})
+});
+
+
