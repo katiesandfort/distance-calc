3eef7b6f80d75c444b9dcf5221110dd1fe311a37
Katie Sandfort <katie.sandfort@gmail.com>
2023-06-15 20:11:28 -0700

    removed icon

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ const path = require('path');
 
 app.use(express.json());
 
-//statically server evertying in the assets folder
-app.use('/assets', express.static(path.join(__dirname, '../assets')));
 // statically serve everything in the build folder on the route '/build'
 app.use('/build', express.static(path.join(__dirname, '../build')));
 // serve index.html on the route '/'
